9b8437252df35e3aab511a5cebc9f233c2060a16
Oguine Christian <christian.oguine@student.howest.be>
2025-10-22 01:06:30 +0200

    initial set up completed

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="p-6">
     <h1 class="text-2xl font-cinzel">Welcome to the App</h1>
-    <p class="mt-2 font-audiowide">This is the main application component.</p>
+    <p class="mt-2 font-font">This is the main application component.</p>
   </div>
 </template>
 
