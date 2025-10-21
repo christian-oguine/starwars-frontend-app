b0e053b1dfbac961ae5d47ec6738aa3208601c79
Oguine Christian <christian.oguine@student.howest.be>
2025-10-21 23:53:30 +0200

    configure googleFonts settings in nuxt.config.ts for enhanced typography

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,5 +7,17 @@ export default defineNuxtConfig({
     '@nuxtjs/google-fonts',
     '@nuxt/icon',
     '@nuxt/image'
-  ]
+  ],
+
+  googleFonts: {
+    families: {
+    'Cinzel Decorative': [400, 700],
+    'Lato': [400, 700, 900]
+    },
+    display: 'swap',
+    preconnect: true,
+    prefetch: true,
+    preload: true,
+    download: true
+  }
 })
